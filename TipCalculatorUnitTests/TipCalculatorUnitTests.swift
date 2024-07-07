75129ecf013d78ad23164846be6b1b1ede30e385
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-07-07 19:03:15 +0530

    Writing Test to Check for Thrown Exceptions

--- TipCalculatorUnitTests/TipCalculatorUnitTests.swift
+++ TipCalculatorUnitTests/TipCalculatorUnitTests.swift
@@ -12,8 +12,21 @@ class when_calculating_tip_based_on_total_amount: XCTestCase {
     func test_should_calculate_tip_successfully() {
         
         let tipCalculator = TipCalculator()
-        let tip = tipCalculator.calculate(total: 100, tipPercentage: 0.1)
+        let tip = try! tipCalculator.calculate(total: 100, tipPercentage: 0.1)
         XCTAssertEqual(10, tip)
+        
+    }
+    
+}
+
+class when_calculating_tip_based_on_negative_total_amount: XCTestCase {
+    
+    func test_should_throw_invalid_input_exception() {
+        
+        let tipCalculator = TipCalculator()
+        XCTAssertThrowsError(try tipCalculator.calculate(total: -100, tipPercentage: 0.1), "") { error in
+            XCTAssertEqual(error as! TipCalculatorError, TipCalculatorError.invalidInput)
+        }
         
     }
     
