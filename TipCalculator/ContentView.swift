9974ef57774c6dcf728e62c03b8944faf6f50a0a
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-07-07 19:16:26 +0530

    Integrating TipCalculator to the SwiftUI App

--- TipCalculator/ContentView.swift
+++ TipCalculator/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
     @State private var tip: String?
     @State private var message: String = ""
     
+    let tipCalculator = TipCalculator()
+    
     var body: some View {
         NavigationView {
             
@@ -32,8 +34,26 @@ struct ContentView: View {
 
                 
                 Button("Calculate Tip") {
-                  
                     
+                    message = ""
+                    tip = ""
+                    
+                    guard let total = Double(self.total) else {
+                        message = "Invalid Input"
+                        return
+                    }
+                    
+                    do {
+                        let result = try tipCalculator.calculate(total: total, tipPercentage: tipPercentage)
+                        let formatter = NumberFormatter()
+                        formatter.numberStyle = .currency
+                        tip = formatter.string(from: NSNumber(value: result))
+                    } catch TipCalculatorError.invalidInput {
+                        message = "Invalid Input"
+                    } catch {
+                        message = error.localizedDescription
+                    }
+               
                 }.padding(.top, 20)
                 
                 Text(message)
