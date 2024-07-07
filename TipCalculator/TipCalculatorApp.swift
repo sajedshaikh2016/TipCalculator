c9c02edb24189289c93dbc26635a24f515604824
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-07-07 17:44:33 +0530

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Sajed Shaikh on 07/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
